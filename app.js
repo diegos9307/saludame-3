a59ee338d49d59e705d643c57030b353851e15d5
diegos9307 <diegos9307@gmail.com>
2022-04-07 20:23:12 -0500

    usando un motor de vistas. handlebars

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 "use strict";
 const express = require("express");
 const path = require("path");
+const hbs = require("express-handlebars");
 const app = express();
+
 const bodyParser = require("body-parser");
 
-app.use("/", express.static(path.join(__dirname, "public")));
-
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set("views", path.join(__dirname, "views"));
+
+app.set(
+  ".hbs",
+  hbs.engine({
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+    defaultLayout: "main",
+  })
+);
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// app.use("/", express.static(path.join(__dirname, "public")));
 
 app.post("/", (req, res) => {
   res.status(200).send(`<h1>Hola ${req.body.name}!<h1/>`);
